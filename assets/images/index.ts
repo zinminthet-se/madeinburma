51f9d851dcf586964de45d7b6ff3ca836a6255c2
zinminthet <zinminthet.se@gmail.com>
2025-10-01 16:24:49 +0630

    initial setup and add image assets

---
+++ assets/images/index.ts
@@ -0,0 +1,33 @@
+import blower from "./blower.png";
+import engine from "./engine.png";
+import diselGenerator from "./dieselgenerator.png";
+import airvent from "./airvent.png";
+import waterpump from "./waterpump.png";
+import iphone from "./iphone.png";
+import samsung from "./samsaung.png";
+import tablet from "./tablet.png";
+import myanmar from "./myanmar.png";
+import gold from "./gold.png";
+import bronze from "./bronze.png";
+import silver from "./silver.png";
+import poster from "./poster.jpeg";
+import approved from "./approved.png";
+import audited from "./audited.png";
+
+export const images = {
+  blower,
+  engine,
+  diselGenerator,
+  airvent,
+  waterpump,
+  iphone,
+  samsung,
+  tablet,
+  myanmar,
+  gold,
+  bronze,
+  silver,
+  poster,
+  approved,
+  audited,
+};
---
+++ src/app/index.ts
@@ -0,0 +1,8 @@
+import { View, Text } from react-native;
+import { SafeAreaView } from react-native-safe-area-context;
+
+const App = () => {
+  return <Text>Hello</Text>;
+};
+
+export default App;
