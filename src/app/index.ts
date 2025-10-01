8a50e2e1a18b78f33c111501e7e0a8a9bb3768a4
zinminthet <zinminthet.se@gmail.com>
2025-10-01 16:31:29 +0630

    rename

--- src/app/index.ts
+++
@@ -1,8 +0,0 @@
-import { View, Text } from react-native;
-import { SafeAreaView } from react-native-safe-area-context;
-
-const App = () => {
-  return <Text>Hello</Text>;
-};
-
-export default App;
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const App = () => {
+  return <View>Hello</View>;
+};
+
+export default App;
